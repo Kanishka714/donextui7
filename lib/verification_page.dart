9638c1f54e0f177b2a571e69717d1ff36cc4376c
Kanishka <kanishkadilshan861@gmail.com>
2024-03-08 00:20:48 +0530

    correct type of pincode textfields dependancies are added

--- lib/verification_page.dart
+++ lib/verification_page.dart
@@ -2,8 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:donextui7/custom_textfield.dart';
 import 'package:donextui7/my_button.dart';
 import 'package:flutter/services.dart';
-import 'package:pin_code_text_field/pin_code_text_field.dart';
-import 'package:flutter/services.dart';
+import 'package:pin_code_fields/pin_code_fields.dart';
 
 class VerificationPage extends StatefulWidget {
   const VerificationPage({Key? key}) : super(key: key);
@@ -13,6 +12,8 @@ class VerificationPage extends StatefulWidget {
 }
 
 class _VerificationPageState extends State<VerificationPage> {
+  String currentText = "";
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -49,153 +50,27 @@ class _VerificationPageState extends State<VerificationPage> {
 
             SizedBox(height: 30.0),
 
-          /*  Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                PinCodeTextField(
-                  keyboardType: TextInputType.number,
-                  maxLength: 4,
-                  autofocus: true,
-                  hideCharacter: false,
-                  highlight: true,
-                  highlightColor: Colors.blue,
-                  defaultBorderColor: Colors.black,
-                  hasTextBorderColor: Colors.white38,
-                  pinBoxWidth: 50,
-                  pinBoxHeight: 50,
-                  pinBoxRadius: 10,
-                  pinBoxDecoration: ProvidedPinBoxDecoration.defaultPinBoxDecoration,
-                  pinTextStyle: TextStyle(fontSize: 22.0),
-                  pinTextAnimatedSwitcherDuration: Duration(milliseconds: 300),
-                  pinTextAnimatedSwitcherTransition: ProvidedPinBoxTextAnimation.scalingTransition,
-                  onTextChanged: (text) {},
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 55.0),
+              child: PinCodeTextField(
+                appContext: context,
+                length: 4,
+                keyboardType: TextInputType.number,
+                animationType: AnimationType.fade,
+                pinTheme: PinTheme(
+                  shape: PinCodeFieldShape.box,
+                  borderRadius: BorderRadius.circular(5),
+                  fieldHeight: 50,
+                  fieldWidth: 50,
+                  activeFillColor: Colors.white,
                 ),
-              ],
-            ),
-            
-           */
-            Form(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  SizedBox(
-                    height: 50,
-                    width: 60,
-                    child: Padding(
-                      padding: const EdgeInsets.only(left: 15.0),
-                      child: Container(
-                        decoration: BoxDecoration(
-                          border: Border.all(color: Colors.black), // Define border style
-                          borderRadius: BorderRadius.circular(8.0), // Optional: Add border radius
-                        ),
-                        child: TextField(
-                          decoration: InputDecoration(
-                            border: InputBorder.none, // Hide default TextField border
-                          ),
-                          style: Theme.of(context).textTheme.headline6,
-                          keyboardType: TextInputType.number,
-                          textAlign: TextAlign.center,
-                          inputFormatters: [
-                            LengthLimitingTextInputFormatter(1),
-                            FilteringTextInputFormatter.digitsOnly,
-                          ],
-                        ),
-                      ),
-                    ),
-                  ),
-
-                  SizedBox(width: 25.0),
-
-                  SizedBox(
-                    height: 50,
-                    width: 60,
-                    child: Padding(
-                      padding: const EdgeInsets.only(left: 15.0),
-                      child: Container(
-                        decoration: BoxDecoration(
-                          border: Border.all(color: Colors.black), // Define border style
-                          borderRadius: BorderRadius.circular(8.0), // Optional: Add border radius
-                        ),
-                        child: TextField(
-                          decoration: InputDecoration(
-                            border: InputBorder.none, // Hide default TextField border
-                          ),
-                          style: Theme.of(context).textTheme.headline6,
-                          keyboardType: TextInputType.number,
-                          textAlign: TextAlign.center,
-                          inputFormatters: [
-                            LengthLimitingTextInputFormatter(1),
-                            FilteringTextInputFormatter.digitsOnly,
-                          ],
-                        ),
-                      ),
-                    ),
-                  ),
-
-                  SizedBox(width: 25.0),
-
-
-                  SizedBox(
-                    height: 50,
-                    width: 60,
-                    child: Padding(
-                      padding: const EdgeInsets.only(left: 15.0),
-                      child: Container(
-                        decoration: BoxDecoration(
-                          border: Border.all(color: Colors.black), // Define border style
-                          borderRadius: BorderRadius.circular(8.0), // Optional: Add border radius
-                        ),
-                        child: TextField(
-                          decoration: InputDecoration(
-                            border: InputBorder.none, // Hide default TextField border
-                          ),
-                          style: Theme.of(context).textTheme.headline6,
-                          keyboardType: TextInputType.number,
-                          textAlign: TextAlign.center,
-                          inputFormatters: [
-                            LengthLimitingTextInputFormatter(1),
-                            FilteringTextInputFormatter.digitsOnly,
-                          ],
-                        ),
-                      ),
-                    ),
-                  ),
-
-                  SizedBox(width: 25.0),
-
-
-                  SizedBox(
-                    height: 50,
-                    width: 60,
-                    child: Padding(
-                      padding: const EdgeInsets.only(left: 15.0),
-                      child: Container(
-                        decoration: BoxDecoration(
-                          border: Border.all(color: Colors.black), // Define border style
-                          borderRadius: BorderRadius.circular(8.0), // Optional: Add border radius
-                        ),
-                        child: TextField(
-                          decoration: InputDecoration(
-                            border: InputBorder.none, // Hide default TextField border
-                          ),
-                          style: Theme.of(context).textTheme.headline6,
-                          keyboardType: TextInputType.number,
-                          textAlign: TextAlign.center,
-                          inputFormatters: [
-                            LengthLimitingTextInputFormatter(1),
-                            FilteringTextInputFormatter.digitsOnly,
-                          ],
-                        ),
-                      ),
-                    ),
-                  ),
-
-                  SizedBox(width: 25.0),
-
-                ],
+                onChanged: (value) {
+                  setState(() {
+                    currentText = value;
+                  });
+                },
               ),
             ),
-
 
             SizedBox(height: 30.0),
 
